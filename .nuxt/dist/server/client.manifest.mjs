8b39b90266301e078d0395081ed9fcdbf96aab77
andreyserdyuk-sys <andrey.serdyuk@creativecode.digital>
2026-05-26 15:07:51 +0530

    remove .nuxt from git

--- .nuxt/dist/server/client.manifest.mjs
+++
@@ -1,1 +0,0 @@
-export { default } from "file:///D:/domains/ratingo/admin-panel/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs"
--- .nuxt/dist/server/server.mjs
+++
@@ -1,1 +0,0 @@
-export { default } from "file:///D:/domains/ratingo/admin-panel/node_modules/@nuxt/vite-builder/dist/runtime/vite-node.mjs"
